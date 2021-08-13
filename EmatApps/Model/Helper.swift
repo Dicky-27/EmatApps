7497d7e9cedf7b479dc61a2492a968a440de72c2
DDHY <ddhyuliani@gmail.com>
2021-08-13 21:30:28 +0700

    add helper func + fix the displayed data on report & monthly report

---
+++ EmatApps/Model/Helper.swift
@@ -0,0 +1,38 @@
+//
+//  Helper.swift
+//  EmatApps
+//
+//  Created by Dian Dinihari on 13/08/21.
+//
+
+import Foundation
+
+
+class Helper {
+    
+    static func rpFormatter(number: Float) -> String {
+        
+        let rupiahFormat = NumberFormatter()
+        
+        rupiahFormat.numberStyle = .decimal
+        rupiahFormat.groupingSeparator = "."
+        rupiahFormat.maximumFractionDigits = 0
+        
+        let finalString = "Rp\(rupiahFormat.string(from: NSNumber(value: number)) ?? "0")"
+        
+        return finalString
+    }
+    
+    static func kwhFormatter(number: Float) -> String {
+        
+        let kwhFormat = NumberFormatter()
+        
+        kwhFormat.numberStyle = .decimal
+        kwhFormat.decimalSeparator = ","
+        kwhFormat.minimumFractionDigits = 2
+        kwhFormat.maximumFractionDigits = 2
+        let finalString = "\(kwhFormat.string(from: NSNumber(value:number)) ?? "0,00") kWh"
+        
+        return finalString
+    }
+}
