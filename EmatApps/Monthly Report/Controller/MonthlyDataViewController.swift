7ba55ca683b0a21ba879795eb022afd719ab5b31
DDHY <ddhyuliani@gmail.com>
2021-08-24 04:09:36 +0700

    get real daily data from DB to daily kwh on Monthly data (old version)

--- EmatApps/Monthly Report/Controller/MonthlyDataViewController.swift
+++ EmatApps/Monthly Report/Controller/MonthlyDataViewController.swift
@@ -34,9 +34,12 @@ class MonthlyDataViewController: UIViewController, UITableViewDelegate, UITableV
     var monthBudget     : Float?
     var highestDaily    : Float = 0.0
     var harga           : Float = 1444.70
+    var dailyDataList   : [Daily_Energies] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        getDailyDataList()
         
         navigationController?.navigationBar.barTintColor = UIColor(named: "DWhite")
         
@@ -60,68 +63,90 @@ class MonthlyDataViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        dayList.count
+        //dayList.count
+        dailyDataList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = dailyUsageTable.dequeueReusableCell(withIdentifier: "dailyUsageCell") as! DailyUsageTableViewCell
         
-        var dailyPow: Float = 0.0
-        let avgPow          = monthDetailPow! / 30.0
-        let maxDailyCost    = highestDaily * harga
-        let maxDailyStr     = maxDailyCost.toRupiahString()
-        
-        if avgPow == 0 {
-            dailyPow = 0.0
-        }
-        else {
-         dailyPow = Float.random(in: avgPow-3.0...avgPow+2.70)
-        }
-        
-        accumulatedPow += dailyPow
-        cell.dayLabel.text = dayList[indexPath.row]
-        
-        if indexPath.row != 29 {
-            cell.dailyKwhLabel.text = dailyPow.toKwhString()
-            if highestDaily < dailyPow { highestDaily = dailyPow }
-        }
-        else {
-            var sisaPow : Float = 0.0
-            if accumulatedPow >= monthDetailPow! {
-                sisaPow = 0.0
+        if dailyDataList.count != 0 {
+            
+            var dailyPow: Float = 0.0
+            let avgPow          = monthDetailPow! / 30.0
+            let maxDailyCost    = highestDaily * harga
+            let maxDailyStr     = maxDailyCost.toRupiahString()
+            
+            if avgPow == 0 {
+                dailyPow = 0.0
             }
             else {
-                sisaPow = monthDetailPow! - accumulatedPow
+                //dailyPow = Float.random(in: avgPow-3.0...avgPow+2.70)
+                dailyPow = dailyDataList[indexPath.row].power ?? 0.0               
             }
             
-            cell.dailyKwhLabel.text = sisaPow.toKwhString()
-            if highestDaily < sisaPow { highestDaily = sisaPow }
-        }
+            accumulatedPow += dailyPow
+            cell.dayLabel.text = dayList[indexPath.row]
+            
+            if indexPath.row != 29 {
+                cell.dailyKwhLabel.text = dailyPow.toKwhString()
+                if highestDaily < dailyPow { highestDaily = dailyPow }
+            }
+            else {
+                var sisaPow : Float = 0.0
+                if accumulatedPow >= monthDetailPow! {
+                    sisaPow = 0.0
+                }
+                else {
+                    sisaPow = monthDetailPow! - accumulatedPow
+                }
+                
+                //cell.dailyKwhLabel.text = sisaPow.toKwhString()
+                cell.dailyKwhLabel.text = dailyPow.toKwhString()
+                if highestDaily < sisaPow { highestDaily = sisaPow }
+            }
 
-        switch dailyPow {
-        case _ where dailyPow == 0:
-            cell.indicatorUsage.backgroundColor = #colorLiteral(red: 0.8705882353, green: 0.8666666667, blue: 0.8666666667, alpha: 1)
-            progressBudget.progress = 0.0
-        case _ where dailyPow < highestDaily:
-            cell.indicatorUsage.backgroundColor = #colorLiteral(red: 0.6039215686, green: 1, blue: 0.4156862745, alpha: 1)
-            progressBudget.progress = 1.0
-        case _ where dailyPow >= highestDaily:
-            cell.indicatorUsage.backgroundColor = #colorLiteral(red: 1, green: 0.3882352941, blue: 0.3529411765, alpha: 1)
-            progressBudget.progress = 0.0
-        default:
-            cell.indicatorUsage.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-            progressBudget.progress = 0.0
-        }
+            switch dailyPow {
+            case _ where dailyPow == 0:
+                cell.indicatorUsage.backgroundColor = #colorLiteral(red: 0.8705882353, green: 0.8666666667, blue: 0.8666666667, alpha: 1)
+                progressBudget.progress = 0.0
+            case _ where dailyPow < highestDaily:
+                cell.indicatorUsage.backgroundColor = #colorLiteral(red: 0.6039215686, green: 1, blue: 0.4156862745, alpha: 1)
+                progressBudget.progress = 1.0
+            case _ where dailyPow >= highestDaily:
+                cell.indicatorUsage.backgroundColor = #colorLiteral(red: 1, green: 0.3882352941, blue: 0.3529411765, alpha: 1)
+                progressBudget.progress = 0.0
+            default:
+                cell.indicatorUsage.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+                progressBudget.progress = 0.0
+            }
 
-        costperDayLabel.text    = maxDailyStr
-        dailyHighestLabel.text  = highestDaily.toKwhString()
+            costperDayLabel.text    = maxDailyStr
+            dailyHighestLabel.text  = highestDaily.toKwhString()
+        }
+        
+        
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // the cell can't be selected on this view
+    }
+    
+    func getDailyDataList(){
+        
+        APIRequest.fetchDailyEnergyData(url: Constant.GET_DAILY_ENERGY_LIST,showLoader: true) { response in
+            // handle response and store it to the data model
+            self.dailyDataList = response
+            
+            DispatchQueue.main.async {
+                self.dailyUsageTable.reloadData()
+            }
+        } failCompletion: { message in
+            print(message)
+        }
     }
 }
 
